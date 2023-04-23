71bb60400d5de33d11f652e4624b85a149e60e02
Omar Ghani <og@Omars-MBP.cust.communityfibre.co.uk>
2023-04-23 06:31:36 +0100

    first changes

---
+++ main.py
@@ -0,0 +1,11 @@
+import asyncio
+from EdgeGPT import Chatbot, ConversationStyle
+
+async def main():
+    bot = Chatbot(cookie_path='./cookies.json')
+    print(await bot.ask(prompt="Hello world", conversation_style=ConversationStyle.creative, wss_link="wss://sydney.bing.com/sydney/ChatHub"))
+    await bot.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
