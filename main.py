9caa5a537813623a0cd618fb7c90b2f7da6baa47
Omar Ghani <og@Omars-MBP.cust.communityfibre.co.uk>
2023-04-23 19:45:16 +0100

    added a while statement to allow the bot to continue answering questions until manually requested to exit

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import re
 from EdgeGPT import Chatbot, ConversationStyle
 
 async def main():
-    bot = Chatbot(cookie_path='./cookies.json')
-    response = await bot.ask(prompt=input("Ask Bing AI: "), conversation_style=ConversationStyle.creative)
-    for message in response["item"]["messages"]:
-        if message["author"] == "bot":
-            bot_response = message["text"]
 
-    bot_response = re.sub('\[\^\d+\^\]', '', bot_response)
+    while True:
+        bot = Chatbot(cookie_path='./cookies.json')
+        response = await bot.ask(prompt=input("Ask Bing AI: "), conversation_style=ConversationStyle.creative)
+        for message in response["item"]["messages"]:
+            if message["author"] == "bot":
+                bot_response = message["text"]
 
-    print("BOT'S RESPONSE:", bot_response)
+        bot_response = re.sub('\[\^\d+\^\]', '', bot_response)
 
-    await bot.close()
+        print("BOT'S RESPONSE:", bot_response)
+
+        await bot.close()
 
 
 if __name__ == "__main__":
